5fe71abf4ca285a9590560c8f2de7a66b0cd6df6
FatemaPatanwala <103182001+FatemaPatanwala@users.noreply.github.com>
2024-07-23 20:17:22 +0530

    Add files via upload

---
+++ guess_a_num_game.py
@@ -0,0 +1,39 @@
+import random
+
+"""print("The computer is guessing a number.")
+# a=random.randint(1,10)
+a=9
+attempt=0
+
+for i in range(0,5):
+    b=int(input("Enter a number:"))
+    attempt+=1
+    if attempt==5:
+        print("You lose")
+    elif b>a:
+        print("Your guessed number is high, TRY A LOWER NUMBER!")
+    elif b<a:
+        print("Your guessed number is low, TRY A GREATER NUMBER!")
+    elif b==a:
+        print("You guessed it correct.")
+        break"""
+
+import random
+
+print("The computer is guessing a number.")
+a=random.randint(1,10)
+
+for i in range(0,5):
+    b=int(input("Enter a number:"))
+    if b==a:
+        print("You guessed it correct. ")
+        break
+    elif i==5:
+        print("You loose")
+    elif b>a:
+        print("Your guessed number is high, TRY A LOWER NUMBER!")
+    elif b<a:
+        print("Your guessed number is low, TRY A GREATER NUMBER!")
+    
+        
+    
